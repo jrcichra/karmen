ff6b2c7c046237eed4b8d052909741d701247622
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2020-09-06 10:07:46 -0400

    Add a second ci event with number

--- ci/python1/python1.py
+++ ci/python1/python1.py
@@ -20,7 +20,7 @@ k.registerAction("action1", action1)
 k.registerAction("action2", action2)
 
 time.sleep(5)
-k.emitEvent("event1",params={"a":"parameter"})
+k.emitEvent("event1",params={"a":"parameter", "b": 5.4})
 time.sleep(5)
 k.emitEvent("event2")
 time.sleep(5)
