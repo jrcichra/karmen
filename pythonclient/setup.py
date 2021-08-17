32fad333cd4c8584f3076fc9bfc8391000fc4dca
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2021-08-16 20:09:02 -0400

    add files to make it a package

---
+++ pythonclient/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="karmen",
+    version="v2.0.2",
+    author="Justin Cichra",
+    author_email="justin@none.com",
+    description="The karmen python library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jrcichra/karmen",
+    project_urls={
+        "Bug Tracker": "https://github.com/jrcichra/karmen/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="."),
+    python_requires=">=3.7",
+)
