b74dd1fe193d6f343839d7fb8ea9e4a67cd35f9c
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2021-12-13 00:32:08 -0500

    mostly done with example

--- rustclient/src/main.rs
+++ rustclient/src/main.rs
@@ -12,6 +12,7 @@ pub mod karmen {
 struct Karmen {
     name: String,
     client: KarmenClient<tonic::transport::Channel>,
+    actions: HashMap<String, fn(parameters: HashMap<String, String>) -> karmen::Result>,
 }
 
 #[tonic::async_trait]
@@ -20,6 +21,16 @@ trait KarmenTraits {
     async fn ping(&mut self) -> Result<String, Box<dyn std::error::Error>>;
     async fn register(&mut self) -> Result<karmen::Result, Box<dyn std::error::Error>>;
     async fn handle_actions(&mut self) -> Result<(), Box<dyn std::error::Error>>;
+    async fn add_action(
+        &mut self,
+        action: fn(parameters: HashMap<String, String>) -> karmen::Result,
+        name: &str,
+    ) -> Result<(), Box<dyn std::error::Error>>;
+    async fn run_event(
+        &mut self,
+        name: &str,
+        params: HashMap<String, String>,
+    ) -> Result<karmen::Result, Box<dyn std::error::Error>>;
 }
 
 #[tonic::async_trait]
@@ -29,6 +40,7 @@ impl KarmenTraits for Karmen {
         Ok(Karmen {
             name: name.to_string(),
             client,
+            actions: HashMap::new(),
         })
     }
     async fn ping(&mut self) -> Result<String, Box<dyn std::error::Error>> {
@@ -42,6 +54,8 @@ impl KarmenTraits for Karmen {
         let request = tonic::Request::new(karmen::RegisterRequest {
             name: self.name.clone(),
             timestamp: SystemTime::now().duration_since(UNIX_EPOCH)?.as_secs() as i64,
+            // no actions or events are sent as part of registration
+            // TODO: investigate if these can be removed from the protobuf definition
             actions: HashMap::new(),
             events: HashMap::new(),
         });
@@ -75,16 +89,68 @@ impl KarmenTraits for Karmen {
         };
         let mut stream = self.client.action_dispatcher(request).await?.into_inner();
         while let Some(res) = stream.message().await? {
-            println!("{:?}", res);
+            println!("Got a request to run {:?}", res);
         }
         Ok(())
+    }
+    async fn add_action(
+        &mut self,
+        action: fn(parameters: HashMap<String, String>) -> karmen::Result,
+        name: &str,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        println!("Adding action {}", name);
+        self.actions.insert(name.to_string(), action);
+        Ok(())
+    }
+    async fn run_event(
+        &mut self,
+        name: &str,
+        params: HashMap<String, String>,
+    ) -> Result<karmen::Result, Box<dyn std::error::Error>> {
+        println!("Running event {}", name);
+        let event = Some(karmen::Event {
+            event_name: name.to_string(),
+            timestamp: SystemTime::now().duration_since(UNIX_EPOCH)?.as_secs() as i64,
+            // TODO: determine if this is used
+            parameters: HashMap::new(),
+        });
+        let request = tonic::Request::new(karmen::EventRequest {
+            event: event,
+            parameters: params,
+            requester_name: self.name.clone(),
+            // TODO: determine if this is used
+            uuid: "".to_string(),
+        });
+        let response = self.client.emit_event(request).await?.into_inner();
+        match response.result {
+            Some(r) => {
+                if r.code != 200 {
+                    println!("{}", r.code);
+                }
+                Ok(r)
+            }
+            None => Err(Box::new(std::io::Error::new(
+                std::io::ErrorKind::Other,
+                "No response",
+            ))),
+        }
+    }
+}
+
+fn a_fn(parameters: HashMap<String, String>) -> karmen::Result {
+    println!("I am running sleep from rust!!!");
+    karmen::Result {
+        code: 200,
+        parameters: parameters,
     }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut client = Karmen::new("bob", "localhost", 8080).await?;
+    client.add_action(a_fn, "sleep").await?;
     client.register().await?;
+    client.run_event("pleaseSleep", HashMap::new()).await?;
     client.handle_actions().await?;
     Ok(())
 }
